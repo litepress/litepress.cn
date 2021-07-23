b23a730950ca80fc25e3f860ad580ae80d97dd7d
Yulinn233 <977869645@qq.com>
2021-07-23 15:09:57 +0800

    新增：ICO图标

--- wp-content/themes/cravatar/header.php
+++ wp-content/themes/cravatar/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="shortcut icon" href="/wp-content/themes/cravatar/assets/img/ico.png" type="image/x-icon" />
     <link rel="profile" href="https://gmpg.org/xfn/11">
     <title><?php wp_title(); ?></title>
     <?php wp_head(); ?>
