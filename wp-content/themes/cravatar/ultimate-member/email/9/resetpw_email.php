b4332c56f06aca127ec08248959dc2e1cd901128
sunxiyuan <sxy@ibadboy.net>
2021-07-27 04:27:52 +0800

    特性(公共头像服务):汉化中级会员的邮件模板

--- wp-content/themes/cravatar/ultimate-member/email/9/resetpw_email.php
+++ wp-content/themes/cravatar/ultimate-member/email/9/resetpw_email.php
@@ -1,15 +1,15 @@
-<div style="max-width: 560px; padding: 20px; background: #ffffff; border-radius: 5px; margin: 40px auto; font-family: Open Sans,Helvetica,Arial; font-size: 15px; color: #666;">
+<div style="max-width: 560px; padding: 20px; background: #ffffff; border-radius: 5px; margin: 40px auto; font-family: BlinkMacSystemFont, 'Helvetica Neue', 'PingFang SC', 'Microsoft YaHei', 'Source Han Sans SC', 'Noto Sans CJK SC', 'WenQuanYi Micro Hei', sans-serif;">
 <div style="color: #444444; font-weight: normal;">
 <div style="text-align: center; font-weight: 600; font-size: 26px; padding: 10px 0; border-bottom: solid 3px #eeeeee;">{site_name}</div>
 <div style="clear: both;"> </div>
 </div>
 <div style="padding: 0 30px 30px 30px; border-bottom: 3px solid #eeeeee;">
-<div style="padding: 30px 0; font-size: 24px; text-align: center; line-height: 40px;">我们收到一个重置您帐户密码的请求。如果您提出此请求，请单击下面的链接更改您的密码:</div>
-<div style="padding: 10px 0 50px 0; text-align: center;"><a style="background: #555555; color: #fff; padding: 12px 30px; text-decoration: none; border-radius: 3px; letter-spacing: 0.3px;" href="{password_reset_link}">重置你的密码</a></div>
-<div style="padding: 15px; background: #eee; border-radius: 3px; text-align: center;">如果您没有提出此请求，请忽略此电子邮件或 <a style="color: #3ba1da; text-decoration: none;" href="mailto:{admin_email}">向我们报告</a>.</div>
+<div style="padding: 30px 0; font-size: 24px; text-align: center; line-height: 40px;">我们收到了重置您帐户密码的请求。如果您提出此请求，请单击以下链接更改您的密码：</div>
+<div style="padding: 10px 0 50px 0; text-align: center;"><a style="background: #0274be; color: #fff; padding: 12px 30px; text-decoration: none; border-radius: 3px; letter-spacing: 0.3px;" href="{password_reset_link}" target="_blank" rel="noopener">重置你的密码</a></div>
+<div style="padding: 15px; background: #eee; border-radius: 3px; text-align: center;">如果您没有提出此请求，请忽略此电子邮件或向我们报告。 <a style="color: #3ba1da; text-decoration: none;" href="mailto:{admin_email}">联系我们</a></div>
 </div>
 <div style="color: #999; padding: 20px 30px;">
-<div>感谢你！</div>
-<div><a style="color: #3ba1da; text-decoration: none;" href="{site_url}">{site_name}</a> </div>
+<div>谢谢您!</div>
+<div><a style="color: #3ba1da; text-decoration: none;" href="{site_url}">{site_name}</a></div>
 </div>
 </div>
