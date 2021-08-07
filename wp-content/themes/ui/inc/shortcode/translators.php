9e30dda331bddf6c92a0062d1c4dc6afdfb1ae97
sunxiyuan <sxy@ibadboy.net>
2021-08-08 01:19:21 +0800

    重构(社区论坛):更改了翻译贡献者的查询语句，同时删除了老wp-china.org上的贡献者名单插件，并将主题中的贡献者短码移动到了专门存放短码的位置上

---
+++ wp-content/themes/ui/inc/shortcode/translators.php
@@ -0,0 +1,31 @@
+<?php
+
+add_shortcode( 'translators', 'wcy_get_translators' );
+
+/**
+ * 译者名单
+ */
+function wcy_get_translators() {
+	$html = '<ul class="translator-list">';
+
+	global $wpdb;
+
+	$translators = wp_cache_get( 'translators', 'litepress-cn' );
+	if ( empty( $translators ) ) {
+		$translators = $wpdb->get_results( 'select user_id, count(*) as count from wp_4_gp_translations where user_id!=517 group by user_id order by count desc;' );
+		wp_cache_set( 'translators', $translators, 'litepress-cn', 86400 );
+	}
+
+	$i = 0;
+	foreach ( $translators as $k => $v ) {
+		if ( $i >= 10 ) {
+			break;
+		}
+		$user_info = get_user_by( 'id', $v->user_id );
+		$html      .= sprintf( '<li><em>%d.</em> <div class="rank-list__name"><a href="/user/%s">%s%s</a></div><span class="rank-list__number">%d 条</span></li>', $k + 1, $user_info->data->user_login, get_avatar( $user_info->data->user_email, 32 ), $user_info->data->display_name, $v->count );
+
+		$i ++;
+	}
+
+	return $html .= '</ul>';
+}
