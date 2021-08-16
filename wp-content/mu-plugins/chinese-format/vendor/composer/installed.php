69b159f806170c0c5ce153b70caecac04c170ebf
sunxiyuan <sxy@ibadboy.net>
2021-08-16 11:31:47 +0800

    特性(全局):中文格式化服务依赖库补全，这个依赖库被更改过以支持php中的i18n字符串

---
+++ wp-content/mu-plugins/chinese-format/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '8f3b10f4dc88bd7a0268943bc7ab3e050d557b57',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '8f3b10f4dc88bd7a0268943bc7ab3e050d557b57',
+            'dev_requirement' => false,
+        ),
+        'naux/auto-correct' => array(
+            'pretty_version' => 'v1.0.3',
+            'version' => '1.0.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../naux/auto-correct',
+            'aliases' => array(),
+            'reference' => 'e1a7796c18a74c1e4b873573f2547627d4dc5b30',
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ wp-content/mu-plugins/chinese-format/vendor/naux/auto-correct/README.md
@@ -0,0 +1,35 @@
+# auto-correct
+自动给中英文之间加入合理的空格并纠正专用名词大小写
+
+## 安装
+首先你需要安装有 `composer`，然执行下面的命令安装：
+```
+composer require naux/auto-correct
+```
+
+## 使用
+```php
+use Naux\AutoCorrect;
+
+$correct = new AutoCorrect;
+
+// 在中英文之间加入合理的空格
+$correct->auto_space("php是世界上最好的语言，之一"); // php 是世界上最好的语言，之一
+
+// 纠正专用词汇大小写
+$correct->auto_correct("php是世界上最好的语言，之一"); // PHP是世界上最好的语言，之一
+
+// 加入空格并纠正词汇（auto_space + auto_correct）
+$correct->convert("php是世界上最好的语言，之一"); // PHP 是世界上最好的语言，之一
+
+//添加外置词典，可添加多个
+echo $auto
+  ->withDict(['hello owlrd' => 'Hello Wrold'])
+  ->withDict(['foo bar' => 'Foo Bar'])
+  ->convert('hello world, foo bar, phphub'); // Hello World, Foo Bar, PHPHub
+```
+
+## 应用案例
+
+[PHPHub](https://phphub.org/) - 整站标题都做了自动转换处理。  
+[Laravel.so](http://laravel.so/) - 整站标题都做了自动转换处理。
