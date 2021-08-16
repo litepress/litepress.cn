2e4cf00073a4cb7035c07b40b1be4e80c6a0429b
sunxiyuan <sxy@ibadboy.net>
2021-08-16 11:33:59 +0800

    特性(翻译平台):翻译记忆库存储重新更改为MySQL 因为ES作为NoSQL数据筛选太蛋疼了。虽然存储在MySQL但是后面会让内容自动同步到ES。也就是说MySQL负责存储，但是ES负责检索

--- wp-content/plugins/wporg-gp-translation-suggestions/inc/class-translation-memory-client.php
+++ wp-content/plugins/wporg-gp-translation-suggestions/inc/class-translation-memory-client.php
@@ -7,6 +7,7 @@ use Text_Diff;
 use WP_Error;
 use WP_Http;
 use WP_Text_Diff_Renderer_inline;
+use function LitePress\Helper\is_chinese;
 
 require_once ABSPATH . '/wp-includes/wp-diff.php';
 
@@ -25,7 +26,9 @@ class Translation_Memory_Client {
 	 * @return true|\WP_Error True on success, WP_Error on failure.
 	 */
 	public static function update( array $translations ) {
-		$requests = [];
+		global $wpdb;
+
+		$memory = [];
 
 		foreach ( $translations as $original_id => $translation_id ) {
 			$translation = GP::$translation->get( $translation_id );
@@ -46,64 +49,45 @@ class Translation_Memory_Client {
 
 			$source = $original->fields()['singular'] ?? '';
 
-			if ( empty( $source ) || empty( $translation->translation_0 ) ) {
-				continue;
-			}
-
 			$source = strtolower( trim( $source ) );
 			$target = trim( $translation->translation_0 );
-
-			$id = md5(
+			$id     = md5(
 				$source
 				. '|'
 				. $target
 			);
 
-			$requests[] = wp_json_encode( array(
-				'index' => array(
-					'_id' => $id,
-				),
-			) );
-			$requests[] = wp_json_encode( array(
-				'id'       => $id,
-				'source'   => $source,
-				'target'   => $target,
-				'priority' => 0,
-			) );
+			$s = $source;
+			$t = $target;
+
+			$is_empty = function ( $str ) {
+				return empty( str_replace( array( '\n', '\r', '\n\r', ' ', '&nbsp;', '&#160;' ), '', $str ) );
+			};
+
+			if ( $is_empty( $s ) || $is_empty( $t ) ) {
+				continue;
+			}
+
+			if ( ! is_chinese( $t ) ) {
+				continue;
+			}
+
+			$memory[ $id ] = $wpdb->prepare( "( '%s', '%s', '%s' )", $id, $source, $target );
 		}
 
-		if ( ! $requests ) {
+		if ( ! $memory ) {
 			return new WP_Error( 'no_translations' );
 		}
 
-		$body = join( PHP_EOL, $requests );
-		$body .= PHP_EOL;
+		$data = join( ',', $memory );
 
-		$request = wp_remote_post(
-			self::API_BULK_ENDPOINT,
-			[
-				'timeout' => 10,
-				'headers' => array(
-					'Content-Type' => 'application/x-ndjson',
-				),
-				'body'    => $body,
-			]
-		);
+		$sql = <<<SQL
+REPLACE INTO wp_4_gp_memory ( id, source, target )
+VALUES 
+$data;
+SQL;
 
-		if ( is_wp_error( $request ) ) {
-			return $request;
-		}
-
-		if ( WP_Http::OK !== wp_remote_retrieve_response_code( $request ) ) {
-			return new WP_Error( 'response_code_not_ok' );
-		}
-
-		$body   = wp_remote_retrieve_body( $request );
-		$result = json_decode( $body, true );
-
-		if ( JSON_ERROR_NONE !== json_last_error() ) {
-			return new WP_Error( 'json_parse_error' );
-		}
+		$result = $wpdb->query( $sql );
 
 		return $result ?: new WP_Error( 'unknown_error' );
 	}
