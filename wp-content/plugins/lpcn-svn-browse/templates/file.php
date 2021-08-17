801731a1ad70fa9995837fe847cfc755f0b08261
sunxiyuan <sxy@ibadboy.net>
2021-08-17 20:26:41 +0800

    修复(SVN文件浏览):浏览器添加缺失的标题

--- wp-content/plugins/lpcn-svn-browse/templates/file.php
+++ wp-content/plugins/lpcn-svn-browse/templates/file.php
@@ -2,8 +2,25 @@
 
 use function LitePress\SVN_Browse\Inc\get_current_dir_path_html;
 
+add_filter('wp_title', function () {
+	return '文件浏览器 - LitePress SVN';
+});
+
 get_header();
 
+/*
+
+ <ul>
+                        <?php
+                        $line = count( explode( PHP_EOL, $body ) );
+
+                        for ( $i = 1; $i <= $line; $i ++ ) {
+                            echo "<li id='L{$i}'></li>";
+                        }
+                        ?>
+                    </ul>
+
+*/
 require __DIR__ . '/header.php';
 ?>
     <main class="container body">
@@ -13,11 +30,12 @@ require __DIR__ . '/header.php';
             </div>
             <div class="container mt-3">
 
-                <?php do_action( 'svn-browse-error-message' ); ?>
+				<?php do_action( 'svn-browse-error-message' ); ?>
 
 				<?php if ( ! empty( $body ) ): ?>
                 <div class="heti my-3">
 				<pre>
+
                     <code class="php"><?php echo esc_html( $body ); ?>
                     </code>
                 </pre>
