aaf71f06c6423eb6f220047af52e90733b1a4cb7
sunxiyuan <sxy@ibadboy.net>
2021-08-17 20:26:41 +0800

    修复(SVN文件浏览):浏览器添加缺失的标题

--- wp-content/plugins/lpcn-svn-browse/templates/dir.php
+++ wp-content/plugins/lpcn-svn-browse/templates/dir.php
@@ -2,6 +2,10 @@
 
 use function LitePress\SVN_Browse\Inc\get_current_dir_path_html;
 
+add_filter('wp_title', function () {
+	return '目录浏览 - LitePress SVN';
+});
+
 get_header();
 
 require __DIR__ . '/header.php';
