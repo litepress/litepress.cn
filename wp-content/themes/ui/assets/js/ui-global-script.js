0b052d946c89ad8e14d51653815e07079ff022bb
Yulinn233 <github>
2021-08-18 15:31:19 +0800

    新增(翻译平台)：前端样式优化

--- wp-content/themes/ui/assets/js/ui-global-script.js
+++ wp-content/themes/ui/assets/js/ui-global-script.js
@@ -99,6 +99,13 @@ $(".wp-code").hover(function () {
 $(".wp-code code").each(function () {
     $(this).html("<ul><li>" + $(this).html().replace(/\n/g, "\n</li><li>") + "\n</li></ul>");
 });
+$(function () {
+var numLi = $(".wp-code .hljs ul li").length;
+console.log(numLi);
+for (var i = 0; i < numLi; i++) {
+    $(".wp-code .hljs ul li").eq(i).wrap('<div  id="L'+ (i + 1) +'" ></div>');
+}
+})
 const n = $(".btn-clipboard");
 n.click(function () {
     $(this).text("已复制");
