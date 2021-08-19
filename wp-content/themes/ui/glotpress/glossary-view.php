fa72e7a17680ecf11b2cd34d67c24511e07df393
sunxiyuan <sxy@ibadboy.net>
2021-08-19 20:46:16 +0800

    特性(翻译平台):优化术语库标题

--- wp-content/themes/ui/glotpress/glossary-view.php
+++ wp-content/themes/ui/glotpress/glossary-view.php
@@ -1,10 +1,10 @@
 <?php
-gp_title( __( 'View Glossary - LitePress翻译平台', 'glotpress' ) );
+gp_title( __( '术语表 - LitePress翻译平台', 'glotpress' ) );
 gp_breadcrumb(
 	array(
 		gp_project_links_from_root( $project ),
 		gp_link_get( gp_url_project_locale( $project->path, $locale->slug, $translation_set->slug ), $translation_set->name ),
-		__( 'Glossary', 'glotpress' ),
+		__( '术语表', 'glotpress' ),
 	)
 );
 
@@ -24,20 +24,24 @@ if ( 0 === $project->id ) {
 	$glossary_title = __( 'Glossary for %s', 'glotpress' );
 }
 ?>
-
-<h2>
+<main class="container">
+    <section class="toolbar">
+<h5>
 	<?php
 	printf(
 		// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 		$glossary_title,
-		esc_html( $translation_set->name ),
+		'简体中文',
 		esc_html( $project->name )
 	);
 	?>
-	<?php gp_link_glossary_edit( $glossary, $translation_set, _x( '(edit)', 'glossary', 'glotpress' ) ); ?>
-	<?php gp_link_glossary_delete( $glossary, $translation_set, _x( '(delete)', 'glossary', 'glotpress' ) ); ?>
-</h2>
 
+</h5>
+        <ul>
+        <?php gp_link_glossary_edit( $glossary, $translation_set, _x( '(edit)', 'glossary', 'glotpress' ) ); ?>
+        <?php gp_link_glossary_delete( $glossary, $translation_set, _x( '(delete)', 'glossary', 'glotpress' ) ); ?>
+        </ul>
+    </section>
 <?php
 /**
  * Filter a glossary description.
@@ -54,8 +58,12 @@ if ( $glossary_description ) {
 	echo '<div class="glossary-description">' . $glossary_description . '</div>';
 }
 ?>
-
-<table class="glossary" id="glossary">
+    <style>
+        body {
+            display: block !important;
+        }
+    </style>
+<table class="glossary translations wp-card " id="glossary">
 	<thead>
 		<tr>
 			<th style="width:20%"><?php _ex( 'Item', 'glossary entry', 'glotpress' ); ?></th>
@@ -91,10 +99,10 @@ if ( $glossary_description ) {
 				<form action="<?php echo esc_url( gp_url_join( $url, '-new' ) ); ?>" method="post">
 					<dl>
 						<dt><label for="new_glossary_entry_term"><?php echo esc_html( _x( 'Original term:', 'glossary entry', 'glotpress' ) ); ?></label></dt>
-						<dd><input type="text" name="new_glossary_entry[term]" id="new_glossary_entry_term" value=""></dd>
+						<dd><input class="form-control" type="text" name="new_glossary_entry[term]" id="new_glossary_entry_term" value=""></dd>
 						<dt><label for="new_glossary_entry_post"><?php _ex( 'Part of speech', 'glossary entry', 'glotpress' ); ?></label></dt>
 						<dd>
-							<select name="new_glossary_entry[part_of_speech]" id="new_glossary_entry_post">
+							<select class="form-select" name="new_glossary_entry[part_of_speech]" id="new_glossary_entry_post">
 							<?php
 								foreach ( GP::$glossary_entry->parts_of_speech as $pos => $name ) {
 									echo "\t<option value='" . esc_attr( $pos ) . "'>" . esc_html( $name ) . "</option>\n";
@@ -103,13 +111,13 @@ if ( $glossary_description ) {
 							</select>
 						</dd>
 						<dt><label for="new_glossary_entry_translation"><?php _ex( 'Translation', 'glossary entry', 'glotpress' ); ?></label></dt>
-						<dd><input type="text" name="new_glossary_entry[translation]" id="new_glossary_entry_translation" value=""></dd>
+						<dd><input class="form-control" type="text" name="new_glossary_entry[translation]" id="new_glossary_entry_translation" value=""></dd>
 						<dt><label for="new_glossary_entry_comments"><?php _ex( 'Comments', 'glossary entry', 'glotpress' ); ?></label></dt>
-						<dd><textarea type="text" name="new_glossary_entry[comment]" id="new_glossary_entry_comments"></textarea></dd>
+						<dd><textarea class="form-control" type="text" name="new_glossary_entry[comment]" id="new_glossary_entry_comments"></textarea></dd>
 					</dl>
 					<p>
 						<input type="hidden" name="new_glossary_entry[glossary_id]" value="<?php echo esc_attr( $glossary->id ); ?>">
-						<input type="submit" name="submit" value="<?php esc_attr_e( 'Create', 'glotpress' ); ?>" id="submit" />
+						<input class="btn btn-primary" type="submit" name="submit" value="<?php esc_attr_e( 'Create', 'glotpress' ); ?>" id="submit" />
 					</p>
 					<?php gp_route_nonce_field( 'add-glossary-entry_' . $project->path . $locale->slug . $translation_set->slug ); ?>
 				</form>
@@ -119,13 +127,14 @@ if ( $glossary_description ) {
 	</tbody>
 </table>
 
-<p class="clear actionlist secondary">
+<section class="clear actionlist secondary wp-card p-3 mb-4">
 	<?php if ( $can_edit ) : ?>
 		<?php echo gp_link( gp_url_join( gp_url_project_locale( $project->path, $locale_slug, $translation_set_slug ), array( 'glossary', '-import' ) ), __( 'Import', 'glotpress' ) ); ?>  &bull;&nbsp;
 	<?php endif; ?>
 
 	<?php echo gp_link( gp_url_join( gp_url_project_locale( $project->path, $locale_slug, $translation_set_slug ), array( 'glossary', '-export' ) ), __( 'Export as CSV', 'glotpress' ) ); ?>
-</p>
+</section>
 
+</main>
 <?php
 gp_tmpl_footer();
