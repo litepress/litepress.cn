ecc43d35a0956df736837db6f89dd25141076e89
sunxiyuan <sxy@ibadboy.net>
2021-08-20 20:26:41 +0800

    特性(全局):HTTP请求类只取失败次数在3次以下的IP。同时设定定时任务每分钟检查代理IP有效性

--- wp-content/mu-plugins/class-wp-http.php
+++ wp-content/mu-plugins/class-wp-http.php
@@ -18,6 +18,56 @@ class WP_Http extends Original_WP_Http {
 
 	const PROXY_IP_POOL_TABLE = 'lp_proxy_ip_pool';
 
+	/**
+	 * 检测坏的代理IP
+	 *
+	 * 并非是标记是坏的就不会再次取用，通常只有累计三次以上才会被抛弃。
+	 */
+	static public function check_all(): bool {
+		$ips     = self::get_all_proxy_ip();
+		$wp_http = new Original_WP_Http();
+
+		foreach ( $ips as $ip ) {
+			$ip = $ip['ip'] ?? '';
+
+			$args = array(
+				'headers' => array(
+					'Host' => 'wordpress.org'
+				),
+				'method'  => 'GET',
+				'timeout' => 20,
+			);
+
+			$r = $wp_http->request( 'http://' . $ip, $args );
+			if ( is_wp_error( $r ) ) {
+				global $wpdb;
+
+				$wpdb->query( "update lp_proxy_ip_pool set bad_num=bad_num+1 where ip = '$ip'" );
+
+				wp_cache_delete( 'proxy_ips' );
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * 获取全部代理IP
+	 */
+	static private function get_all_proxy_ip(): array {
+		global $wpdb;
+
+		$ips = wp_cache_get( 'proxy_ips' );
+		if ( empty( $ips ) ) {
+			$sql = sprintf( 'select ip from %s where bad_num < 3', self::PROXY_IP_POOL_TABLE );
+			$ips = $wpdb->get_results( $sql, ARRAY_A );
+
+			wp_cache_set( 'proxy_ips', $ips, '', 84000 );
+		}
+
+		return $ips;
+	}
+
 	public function request( $url, $args = array() ): WP_Error|array {
 		$allowed = array(
 			'translate.google.cn',
@@ -41,16 +91,9 @@ class WP_Http extends Original_WP_Http {
 	 * @return string|false
 	 */
 	static public function get_proxy_ip(): string|false {
-		global $wpdb;
+		$ips = self::get_all_proxy_ip();
 
-		$ips = wp_cache_get( 'proxy_ips' );
-		if ( empty( $ips ) ) {
-			$ips = $wpdb->get_results( 'select ip from ' . self::PROXY_IP_POOL_TABLE );
-
-			wp_cache_set( 'proxy_ips', $ips, '', 84000 );
-		}
-
-		return $ips[ rand( 0, count( $ips ) - 1 ) ]->ip ?? false;
+		return $ips[ rand( 0, count( $ips ) - 1 ) ]['ip'] ?? false;
 	}
 
 }
@@ -84,3 +127,17 @@ function wp_remote_post( $url, $args = array() ): WP_Error|array {
 
 	return $http->request( $url, $args );
 }
+
+// 定期检查代理IP是否有损坏的
+add_action( 'plugins_loaded', function () {
+	global $blog_id;
+
+	if ( 1 === (int) $blog_id ) {
+		add_action( 'lpcn_check_all_proxy_ip', array( WP_Http::class, 'check_all' ) );
+
+		$timestamp = wp_next_scheduled( 'lpcn_check_all_proxy_ip' );
+		if ( empty( $timestamp ) ) {
+			wp_schedule_event( time(), 'hourly', 'lpcn_check_all_proxy_ip' );
+		}
+	}
+} );
