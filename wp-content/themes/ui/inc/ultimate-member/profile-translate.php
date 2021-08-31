765c609c7d9cede945176c9b99aff4b5139c613f
sunxiyuan <sxy@ibadboy.net>
2021-08-31 13:34:14 +0800

    特性(社区论坛):用户资料卡的翻译详情支持分页与查询（前端待实现）

--- wp-content/themes/ui/inc/ultimate-member/profile-translate.php
+++ wp-content/themes/ui/inc/ultimate-member/profile-translate.php
@@ -26,8 +26,11 @@ function get_gp_manage_projects( int $user_id, bool $is_count = false ): array|i
 		$project_ids[] = explode( '|', $permission['object_id'] )[0];
 	}
 
+	$search = $_GET['ts'] ?? '';
+	$paged  = $_GET['paged'] ?? 1;
+
 	$project_ids = join( ',', $project_ids );
-	$sql         = "select id, name, author, slug, path, description, parent_project_id from wp_4_gp_projects where 1=1 and id in ( {$project_ids} ) and active=1;";
+	$sql         = $wpdb->prepare( "select id, name, author, slug, path, description, parent_project_id from wp_4_gp_projects where 1=1 and id in ( {$project_ids} ) and active=1 and name like '%%%s%%' limit %d,%d;;", $search, $paged * 15, 15 );
 
 	return $wpdb->get_results( $sql, ARRAY_A );
 }
@@ -56,16 +59,20 @@ function get_gp_contribution_projects( int $user_id, bool $is_count = false ): a
 		$translation_set_ids[] = $translation['translation_set_id'];
 	}
 
+	$search = $_GET['ts'] ?? '';
+	$paged  = $_GET['paged'] ?? 1;
+
 	$translation_set_ids = join( ',', $translation_set_ids );
-	$sql                 = <<<SQL
+	$sql                 = sprintf( <<<SQL
 select *
 from wp_4_gp_projects
 where 1 = 1
   and id in (select parent_project_id
              from wp_4_gp_projects
-             where id in (select project_id from wp_4_gp_translation_sets where id in ({$translation_set_ids})));
-SQL;
-
+             where id in (select project_id from wp_4_gp_translation_sets where id in ({$translation_set_ids})))
+ and name like '%%%s%%'
+ limit %d,%d;
+SQL, $search, $paged * 15, 15 );
 
 	return $wpdb->get_results( $sql, ARRAY_A );
 }
