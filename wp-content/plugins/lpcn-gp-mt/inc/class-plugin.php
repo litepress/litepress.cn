f6bd4b62ecb1b6184349759e6d1678cf73d05c9f
sunxiyuan <sxy@ibadboy.net>
2021-09-14 16:36:49 +0800

    特性(翻译平台):经过初步调教的机器翻译填充功能

--- wp-content/plugins/lpcn-gp-mt/inc/class-plugin.php
+++ wp-content/plugins/lpcn-gp-mt/inc/class-plugin.php
@@ -3,6 +3,7 @@
 namespace LitePress\GlotPress\MT;
 
 use GP;
+use GP_Route;
 
 class Plugin {
 
@@ -19,6 +20,18 @@ class Plugin {
 	}
 
 	/**
+	 * Initializes the plugin.
+	 */
+	public function plugins_loaded() {
+		GP::$router->add( "/gp-mt/(.+?)", array( Translate::class, 'schedule_gp_mt' ), 'get' );
+		GP::$router->add( "/gp-mt/(.+?)", array( Translate::class, 'schedule_gp_mt' ), 'post' );
+		//if ( isset( $_GET['testo'] ) ) {
+		//add_action( 'gp_originals_imported', array( $this, 'schedule_gp_mt' ), 999 );
+		add_action( 'lpcn_schedule_gp_mt', array( Translate::class, 'job' ), 999, 3 );
+		//}
+	}
+
+	/**
 	 * Returns always the same instance of this plugin.
 	 *
 	 * @return Plugin
@@ -31,85 +44,6 @@ class Plugin {
 		return self::$instance;
 	}
 
-	/**
-	 * Initializes the plugin.
-	 */
-	public function plugins_loaded() {
-			//add_action( 'gp_originals_imported', array( $this, 'schedule_gp_mt' ), 999 );
-			//add_action( 'lpcn_schedule_gp_mt', array( Translate::class, 'job' ), 999, 3 );
-	}
 
-	/**
-	 * 创建机器翻译填充任务
-	 */
-	public function schedule_gp_mt( $project_id ) {
-		$project_id = (int) $project_id;
-
-		$project = GP::$project->find_one( array( 'id' => $project_id ) )->fields();
-
-		$project_name = $this->get_name_by_project_id( $project['id'] );
-
-		// 获取待翻译原文
-		$sql = <<<SQL
-select *
-from wp_4_gp_originals
-where project_id = {$project_id}
-  and id not in (
-    select original_id
-    from wp_4_gp_translations
-    where translation_set_id = (
-        select id
-        from wp_4_gp_translation_sets
-        where project_id = {$project_id}
-    )
-);
-SQL;
-
-		$originals = GP::$original->many( $sql );
-
-		$excluded = array(
-			$project_name
-		);
-		for ( $i = 0; true; $i += 100 ) {
-			$item = array_slice( $originals, $i, 100, true );
-			if ( empty( $item ) ) {
-				break;
-			}
-
-			//do_action('lpcn_schedule_gp_mt' , $project_id, $item, $excluded );
-			wp_schedule_single_event( time() + 60, 'lpcn_schedule_gp_mt', [
-				'project_id' => $project_id,
-				'originals'  => $item,
-				'excluded'   => $excluded
-			] );
-		}
-		var_dump( 'over' );
-		exit;
-	}
-
-	/**
-	 * 通过项目ID获取项目名
-	 *
-	 * 这不是简单的读取项目属性，因为项目属性中的项目名通常包含了长尾副词，所以这个函数尝试用项目原文中分析项目名称
-	 */
-	private function get_name_by_project_id( int $project_id ): string {
-		$allowed = array(
-			'Theme Name of the theme',
-			'Plugin Name of the plugin',
-			'Name of the plugin',
-			'Name of the theme',
-		);
-
-		$original = GP::$original->find_one( array(
-			'project_id' => $project_id,
-			'comment'    => $allowed,
-		) );
-
-		if ( $original ) {
-			return $original->singular;
-		} else {
-			return '';
-		}
-	}
 
 }
