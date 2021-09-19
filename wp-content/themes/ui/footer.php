20697a993c59a7cd8fe11a3a9daf368aa2675897
Yulinn233 <github>
2021-09-19 08:54:42 +0800

    修复(翻译平台)：新增翻译JS  更新首页插件版本号

--- wp-content/themes/ui/footer.php
+++ wp-content/themes/ui/footer.php
@@ -9,7 +9,7 @@
             <div class="modal-body">
                 <main class="form-signin">
                     <form>
-                        <img class="mb-4" src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+                        <img class="mb-4" src="" alt="" width="72" height="57">
                         <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
                         <div class="form-floating">
