070a9e78ca125f9f129f3039b1e4c9ffc98510dc
sunxiyuan <sxy@ibadboy.net>
2021-09-20 09:42:28 +0800

    特性(翻译平台):消息通知插件暂存（未完成）

--- wp-content/plugins/lpcn-gp-notify/inc/class-plugin.php
+++ wp-content/plugins/lpcn-gp-notify/inc/class-plugin.php
@@ -35,8 +35,12 @@ class Plugin {
 	 * Initializes the plugin.
 	 */
 	public function plugins_loaded() {
-		//add_action( 'gp_translation_saved', array( $this, 'gp_translation_saved' ), 10, 2 );
-		//add_action('gp_original_saved');
+		$user = wp_get_current_user();
+		if ( 1 === $user->ID ) {
+			//add_action( 'gp_translation_saved', array( $this, 'gp_translation_saved' ), 10, 2 );
+			//add_action( 'gp_original_saved' );
+		}
+
 
 		add_action( 'lpcn_gp_notify', array( $this, 'send' ) );
 	}
@@ -78,7 +82,7 @@ class Plugin {
 		return true;
 	}
 
-	private function add_cron( string $title, string $content, array $to_emails ) {
+	private function add_cron( string $title, string $content, array $to_emails, int $time ) {
 		$timestamp = time();
 
 		//do_action( 'lpcn_gp_notify', $avatar_url, $avatar_hash, $email_hash );
@@ -89,10 +93,10 @@ class Plugin {
 		);
 		$next = wp_next_scheduled( 'lpcn_gp_notify', $args );
 		if ( empty( $next ) ) { // 如果未安排任务或者任务过期就安排之
-			wp_schedule_single_event( time() + 3600, 'lpcn_gp_notify', $args );
+			wp_schedule_single_event( time() + $time, 'lpcn_gp_notify', $args );
 		} elseif ( ( $next - $timestamp ) < 600 ) { // 如果距离过期不足十分钟，则删除并重新安排
 			wp_clear_scheduled_hook( 'lpcn_gp_notify', $args );
-			wp_schedule_single_event( time() + 3600, 'lpcn_gp_notify', $args );
+			wp_schedule_single_event( time() + $time, 'lpcn_gp_notify', $args );
 		}
 	}
 
