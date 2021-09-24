b7774009862e0c8f234358ca4d4604a498e1fcd5
sunxiyuan <sxy@ibadboy.net>
2021-09-24 20:34:54 +0800

    特性(UI主题):将翻译平台所述的静态文件限制只在翻译平台下加载

--- wp-content/themes/ui/inc/enqueue-scripts.php
+++ wp-content/themes/ui/inc/enqueue-scripts.php
@@ -4,68 +4,61 @@ global $blog_id;
 /**
  * 全局静态文件引入
  */
-add_action('wp_enqueue_scripts', function () {
-    wp_enqueue_style('bootstrap', get_stylesheet_directory_uri() . '/assets/css/bootstrap.min.css');
-    wp_enqueue_style('ui-global-style', get_stylesheet_directory_uri() . '/assets/css/ui-global-style.css');
-    wp_enqueue_style('fontawesome', get_stylesheet_directory_uri() . '/assets/fontawesome/css/all.min.css');
-    wp_enqueue_style('agate', get_stylesheet_directory_uri() . '/assets/css/agate.min.css');
-    wp_enqueue_style('heti', get_stylesheet_directory_uri() . '/assets/css/heti.min.css');
-    wp_enqueue_style('ui-member', get_stylesheet_directory_uri() . '/assets/css/ui-member.css');
-	wp_enqueue_style('nprogress', get_stylesheet_directory_uri() . '/assets/css/nprogress.min.css');
-	wp_enqueue_style('lightgallery', get_stylesheet_directory_uri() . '/assets/css/lightgallery-bundle.min.css');
+add_action( 'wp_enqueue_scripts', function () {
+	wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() . '/assets/css/bootstrap.min.css' );
+	wp_enqueue_style( 'ui-global-style', get_stylesheet_directory_uri() . '/assets/css/ui-global-style.css' );
+	wp_enqueue_style( 'fontawesome', get_stylesheet_directory_uri() . '/assets/fontawesome/css/all.min.css' );
+	wp_enqueue_style( 'agate', get_stylesheet_directory_uri() . '/assets/css/agate.min.css' );
+	wp_enqueue_style( 'heti', get_stylesheet_directory_uri() . '/assets/css/heti.min.css' );
+	wp_enqueue_style( 'ui-member', get_stylesheet_directory_uri() . '/assets/css/ui-member.css' );
+	wp_enqueue_style( 'nprogress', get_stylesheet_directory_uri() . '/assets/css/nprogress.min.css' );
+	wp_enqueue_style( 'lightgallery', get_stylesheet_directory_uri() . '/assets/css/lightgallery-bundle.min.css' );
 
-	wp_enqueue_script('lightgallery', get_stylesheet_directory_uri() . '/assets/js/lightgallery.umd.js', [], false, true);
-	wp_enqueue_script('nprogress', get_stylesheet_directory_uri() . '/assets/js/nprogress.min.js', [], false, true);
-    wp_enqueue_script('highlight', get_stylesheet_directory_uri() . '/assets/js/highlight.min.js', [], false, true);
-    wp_enqueue_script('clipboard', get_stylesheet_directory_uri() . '/assets/js/clipboard.min.js', [], false, true);
-    wp_enqueue_script('ui-global-script', get_stylesheet_directory_uri() . '/assets/js/ui-global-script.js', ['jquery'], false, true);
-    wp_enqueue_script('bootstrap-bundle', get_stylesheet_directory_uri() . '/assets/js/bootstrap.bundle.min.js', ['jquery'], false, true);
-    //wp_enqueue_script('heti-addon', get_stylesheet_directory_uri() . '/assets/js/heti-addon.min.js', [], false, true);
-    //wp_add_inline_script('heti-addon', 'const heti = new Heti(".heti");heti.autoSpacing();');
-	//wp_enqueue_script('instantclick', get_stylesheet_directory_uri() . '/assets/js/instantclick.min.js', [], false, true);
-	//wp_add_inline_script('instantclick', 'InstantClick.init();');
-
-	//wp_deregister_script('gp-common');
-	wp_enqueue_script('ui-glotpress', get_stylesheet_directory_uri() . '/assets/js/ui-glotpress.js', [], false, true);
-});
+	wp_enqueue_script( 'lightgallery', get_stylesheet_directory_uri() . '/assets/js/lightgallery.umd.js', [], false, true );
+	wp_enqueue_script( 'nprogress', get_stylesheet_directory_uri() . '/assets/js/nprogress.min.js', [], false, true );
+	wp_enqueue_script( 'highlight', get_stylesheet_directory_uri() . '/assets/js/highlight.min.js', [], false, true );
+	wp_enqueue_script( 'clipboard', get_stylesheet_directory_uri() . '/assets/js/clipboard.min.js', [], false, true );
+	wp_enqueue_script( 'ui-global-script', get_stylesheet_directory_uri() . '/assets/js/ui-global-script.js', [ 'jquery' ], false, true );
+	wp_enqueue_script( 'bootstrap-bundle', get_stylesheet_directory_uri() . '/assets/js/bootstrap.bundle.min.js', [ 'jquery' ], false, true );
+} );
 
 /**
  * bbpress静态文件引入，同时去除其自带的样式
  */
-add_filter('bbp_default_styles', function () {
-    wp_enqueue_style('ui-bbpress', get_stylesheet_directory_uri() . '/assets/css/ui-bbpress.css');
-    wp_enqueue_script('ui-bbpress', get_stylesheet_directory_uri() . '/assets/js/ui-bbpress.js', [
-        'jquery',
-        'bootstrap-bundle',
-        'highlight',
-        'clipboard'
-    ], false, true);
+add_filter( 'bbp_default_styles', function () {
+	wp_enqueue_style( 'ui-bbpress', get_stylesheet_directory_uri() . '/assets/css/ui-bbpress.css' );
+	wp_enqueue_script( 'ui-bbpress', get_stylesheet_directory_uri() . '/assets/js/ui-bbpress.js', [
+		'jquery',
+		'bootstrap-bundle',
+		'highlight',
+		'clipboard'
+	], false, true );
 
-    return array();
-});
+	return array();
+} );
 
 /**
  * 翻译平台静态文件引入
  */
-add_action('gp_init', function () {
-    wp_enqueue_style('ui-glotpress', get_stylesheet_directory_uri() . '/assets/css/ui-glotpress.css');
-});
+add_action( 'gp_init', function () {
+	wp_enqueue_style( 'ui-glotpress', get_stylesheet_directory_uri() . '/assets/css/ui-glotpress.css' );
+} );
 
 /**
  * Woo商城静态文件引入
  */
-add_action('woocommerce_init', function () {
-    wp_enqueue_style('ui-woo', get_stylesheet_directory_uri() . '/assets/css/ui-woo.css');
-    wp_enqueue_script('ui-woo', get_stylesheet_directory_uri() . '/assets/js/ui-woo.js', ['jquery'], false, true);
-    wp_enqueue_script('urljs', get_stylesheet_directory_uri() . '/assets/js/url.min.js', [], false, true);
-});
+add_action( 'woocommerce_init', function () {
+	wp_enqueue_style( 'ui-woo', get_stylesheet_directory_uri() . '/assets/css/ui-woo.css' );
+	wp_enqueue_script( 'ui-woo', get_stylesheet_directory_uri() . '/assets/js/ui-woo.js', [ 'jquery' ], false, true );
+	wp_enqueue_script( 'urljs', get_stylesheet_directory_uri() . '/assets/js/url.min.js', [], false, true );
+} );
 
 /**
  * 应用市场“我的账户”页静态文件引入
  */
-add_action('woocommerce_account_content', function () {
-    wp_enqueue_style('ui-woo-my-account', get_stylesheet_directory_uri() . '/assets/css/ui-woo-my-account.css');
-});
+add_action( 'woocommerce_account_content', function () {
+	wp_enqueue_style( 'ui-woo-my-account', get_stylesheet_directory_uri() . '/assets/css/ui-woo-my-account.css' );
+} );
 
 /**
  * 删除终极会员的css和js
@@ -122,14 +115,14 @@ add_action('wp_enqueue_scripts', function () {
 }, 999);
 */
 
-add_action('wp_enqueue_scripts', function () {
-    if (!function_exists('is_product_category') || !function_exists('is_product_tag')) {
-        return;
-    }
+add_action( 'wp_enqueue_scripts', function () {
+	if ( ! function_exists( 'is_product_category' ) || ! function_exists( 'is_product_tag' ) ) {
+		return;
+	}
 
-    global $wp;
-    if (is_product_category() || is_product_tag() || isset($wp->query_vars['wcpv_product_vendors'])) {
-        $data = <<<html
+	global $wp;
+	if ( is_product_category() || is_product_tag() || isset( $wp->query_vars['wcpv_product_vendors'] ) ) {
+		$data = <<<html
 	    $.each(categories,function (i,val) {
 	        var urlsearch = $(location).attr('search');
 	        var url_noparm = location.protocol + '//' + location.host + location.pathname;
@@ -170,19 +163,30 @@ add_action('wp_enqueue_scripts', function () {
 	        })
 	    });
 html;
-        wp_add_inline_script('ui-woo', $data);
-    }
-});
+		wp_add_inline_script( 'ui-woo', $data );
+	}
+} );
 
-if (6 === (int)$blog_id) {
-    add_action('wp_enqueue_scripts', function () {
-        wp_enqueue_style('ui-docs', get_stylesheet_directory_uri() . '/assets/css/ui-docs.css', array('wedocs-styles'));
-        wp_enqueue_script('ui-docs', get_stylesheet_directory_uri() . '/assets/js/ui-docs.js', array('jquery'));
-    });
+
+// 翻译平台依赖的静态资源
+if ( 4 === (int) $blog_id ) {
+	add_action( 'wp_enqueue_scripts', function () {
+		wp_enqueue_script( 'ui-glotpress', get_stylesheet_directory_uri() . '/assets/js/ui-glotpress.js', [], false, true );
+	} );
 }
 
-if (8 === (int)$blog_id) {
-    add_action('wp_enqueue_scripts', function () {
-        wp_enqueue_style('needs', get_stylesheet_directory_uri() . '/assets/css/needs.css');
-    });
+// 开发文档依赖的静态资源
+if ( 6 === (int) $blog_id ) {
+	add_action( 'wp_enqueue_scripts', function () {
+		wp_enqueue_style( 'ui-docs', get_stylesheet_directory_uri() . '/assets/css/ui-docs.css', array( 'wedocs-styles' ) );
+		wp_enqueue_script( 'ui-docs', get_stylesheet_directory_uri() . '/assets/js/ui-docs.js', array( 'jquery' ) );
+	} );
+}
+
+
+// 需求统计依赖的静态资源
+if ( 8 === (int) $blog_id ) {
+	add_action( 'wp_enqueue_scripts', function () {
+		wp_enqueue_style( 'needs', get_stylesheet_directory_uri() . '/assets/css/needs.css' );
+	} );
 }
