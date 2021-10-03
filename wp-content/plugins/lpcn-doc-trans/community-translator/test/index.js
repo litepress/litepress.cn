5920104f3316bec01cb5d98112765fd1903c6748
sunxiyuan <sxy@ibadboy.net>
2021-10-03 14:42:50 +0800

    特性(文档平台):前端翻译插件重构，移除对 wordpress.com 的社区翻译的依赖

--- wp-content/plugins/lpcn-doc-trans/community-translator/test/index.js
+++
@@ -1,9 +0,0 @@
-console.log( 'Running unit tests' );
-
-require( "./glotpress" );
-require( "./translation-pair" );
-require( "./string-extraction/translation-pair" );
-require( "./de" );
-require( "./ru" );
-require( "./jp" );
-require( "./batcher.js" );
