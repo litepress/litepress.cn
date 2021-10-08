7941f7111181f91813e2bddf5db05816db988902
sunxiyuan <sxy@ibadboy.net>
2021-10-08 18:08:34 +0800

    重构(翻译平台):机器翻译引擎重构，以为 API 的开发打基础。同时改进了对 HTML 标签的支持，并去除了词性标记功能（主要是太占系统资源，不占资源的方案准确度又不行）

---
+++ wp-content/plugins/lpcn-gp-mt/inc/class-web.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace LitePress\GlotPress\MT;
+
+use GP;
+use GP_Route;
+
+class Web {
+
+	/**
+	 * 创建网页端翻译任务
+	 */
+	public function add_web_translate_job( $project_id ) {
+		$project = GP::$project->find_one( array( 'id' => $project_id ) )->fields();
+
+		// 获取待翻译原文
+		$sql = <<<SQL
+select *
+from wp_4_gp_originals
+where project_id = {$project_id}
+  and id not in (
+    select original_id
+    from wp_4_gp_translations
+    where translation_set_id = (
+        select id
+        from wp_4_gp_translation_sets
+        where project_id = {$project_id}
+    )
+)
+  and status = '+active';
+SQL;
+
+		$originals = GP::$original->many( $sql );
+
+		for ( $i = 0; true; $i += 300 ) {
+			$item = array_slice( $originals, $i, 500, true );
+			if ( empty( $item ) ) {
+				break;
+			}
+
+			//do_action( 'lpcn_schedule_gp_mt', $project_id, $item );
+
+			wp_schedule_single_event( time() + 1, 'lpcn_schedule_gp_mt', [
+				'project_id' => $project_id,
+				'originals'  => $item,
+			] );
+		}
+
+		$referer = gp_url_project( $project['path'] );
+		if ( isset( $_SERVER['HTTP_REFERER'] ) ) {
+			$referer = $_SERVER['HTTP_REFERER'];
+		}
+
+		$route            = new GP_Route();
+		$route->notices[] = '该请求已加入队列，请稍后刷新页面';
+		$route->redirect( $referer );
+	}
+
+	public function before_request() {
+	}
+
+	public function after_request() {
+	}
+
+}
