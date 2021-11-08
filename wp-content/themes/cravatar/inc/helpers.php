c5e62c99510948a44306d85a16a8894ce56662e4
sunxiyuan <sxy@ibadboy.net>
2021-11-08 16:14:27 +0800

    修复(Cravatar):邮件使用 HTML 模式发送后换行符失效的问题

--- wp-content/themes/cravatar/inc/helpers.php
+++ wp-content/themes/cravatar/inc/helpers.php
@@ -54,9 +54,9 @@ function send_email_for_bind_email( string $address ): bool {
 	$subject = '添加新邮箱到你的 Cravatar 账号';
 	$message = <<<html
 你好 {$user->display_name}:
-
+<br/>
 这是你的邮箱激活地址:<a href="{$active_url}">{$active_url}</a>
-
+<br/>
 此激活地址有效期 10 分钟哦
 html;
 
