30bfd0f4009341f33a481945ce9554fde6a40bd0
sunxiyuan <sxy@ibadboy.net>
2021-11-09 14:13:04 +0800

    特性(全局):新增 Contact Form 7 表单插件

---
+++ wp-content/plugins/contact-form-7/modules/file.php
@@ -0,0 +1,185 @@
+<?php
+/**
+** A base module for [file] and [file*]
+**/
+
+/* form_tag handler */
+
+add_action( 'wpcf7_init', 'wpcf7_add_form_tag_file', 10, 0 );
+
+function wpcf7_add_form_tag_file() {
+	wpcf7_add_form_tag( array( 'file', 'file*' ),
+		'wpcf7_file_form_tag_handler',
+		array(
+			'name-attr' => true,
+			'file-uploading' => true,
+		)
+	);
+}
+
+function wpcf7_file_form_tag_handler( $tag ) {
+	if ( empty( $tag->name ) ) {
+		return '';
+	}
+
+	$validation_error = wpcf7_get_validation_error( $tag->name );
+
+	$class = wpcf7_form_controls_class( $tag->type );
+
+	if ( $validation_error ) {
+		$class .= ' wpcf7-not-valid';
+	}
+
+	$atts = array();
+
+	$atts['size'] = $tag->get_size_option( '40' );
+	$atts['class'] = $tag->get_class_option( $class );
+	$atts['id'] = $tag->get_id_option();
+	$atts['tabindex'] = $tag->get_option( 'tabindex', 'signed_int', true );
+
+	$atts['accept'] = wpcf7_acceptable_filetypes(
+		$tag->get_option( 'filetypes' ), 'attr' );
+
+	if ( $tag->is_required() ) {
+		$atts['aria-required'] = 'true';
+	}
+
+	if ( $validation_error ) {
+		$atts['aria-invalid'] = 'true';
+		$atts['aria-describedby'] = wpcf7_get_validation_error_reference(
+			$tag->name
+		);
+	} else {
+		$atts['aria-invalid'] = 'false';
+	}
+
+	$atts['type'] = 'file';
+	$atts['name'] = $tag->name;
+
+	$atts = wpcf7_format_atts( $atts );
+
+	$html = sprintf(
+		'<span class="wpcf7-form-control-wrap %1$s"><input %2$s />%3$s</span>',
+		sanitize_html_class( $tag->name ), $atts, $validation_error
+	);
+
+	return $html;
+}
+
+
+/* Validation + upload handling filter */
+
+add_filter( 'wpcf7_validate_file', 'wpcf7_file_validation_filter', 10, 3 );
+add_filter( 'wpcf7_validate_file*', 'wpcf7_file_validation_filter', 10, 3 );
+
+function wpcf7_file_validation_filter( $result, $tag, $args ) {
+	$args = wp_parse_args( $args, array() );
+
+	if ( isset( $args['uploaded_files'] ) ) {
+		$maybe_error = $args['uploaded_files'];
+
+		if ( is_wp_error( $maybe_error ) ) {
+			$result->invalidate( $tag, $maybe_error->get_error_message() );
+		}
+	}
+
+	return $result;
+}
+
+add_filter( 'wpcf7_mail_tag_replaced_file', 'wpcf7_file_mail_tag', 10, 4 );
+add_filter( 'wpcf7_mail_tag_replaced_file*', 'wpcf7_file_mail_tag', 10, 4 );
+
+function wpcf7_file_mail_tag( $replaced, $submitted, $html, $mail_tag ) {
+	$submission = WPCF7_Submission::get_instance();
+	$uploaded_files = $submission->uploaded_files();
+	$name = $mail_tag->field_name();
+
+	if ( ! empty( $uploaded_files[$name] ) ) {
+		$paths = (array) $uploaded_files[$name];
+		$paths = array_map( 'wp_basename', $paths );
+
+		$replaced = wpcf7_flat_join( $paths );
+	}
+
+	return $replaced;
+}
+
+
+/* Tag generator */
+
+add_action( 'wpcf7_admin_init', 'wpcf7_add_tag_generator_file', 50, 0 );
+
+function wpcf7_add_tag_generator_file() {
+	$tag_generator = WPCF7_TagGenerator::get_instance();
+	$tag_generator->add( 'file', __( 'file', 'contact-form-7' ),
+		'wpcf7_tag_generator_file' );
+}
+
+function wpcf7_tag_generator_file( $contact_form, $args = '' ) {
+	$args = wp_parse_args( $args, array() );
+	$type = 'file';
+
+	$description = __( "Generate a form-tag for a file uploading field. For more details, see %s.", 'contact-form-7' );
+
+	$desc_link = wpcf7_link( __( 'https://contactform7.com/file-uploading-and-attachment/', 'contact-form-7' ), __( 'File uploading and attachment', 'contact-form-7' ) );
+
+?>
+<div class="control-box">
+<fieldset>
+<legend><?php echo sprintf( esc_html( $description ), $desc_link ); ?></legend>
+
+<table class="form-table">
+<tbody>
+	<tr>
+	<th scope="row"><?php echo esc_html( __( 'Field type', 'contact-form-7' ) ); ?></th>
+	<td>
+		<fieldset>
+		<legend class="screen-reader-text"><?php echo esc_html( __( 'Field type', 'contact-form-7' ) ); ?></legend>
+		<label><input type="checkbox" name="required" /> <?php echo esc_html( __( 'Required field', 'contact-form-7' ) ); ?></label>
+		</fieldset>
+	</td>
+	</tr>
+
+	<tr>
+	<th scope="row"><label for="<?php echo esc_attr( $args['content'] . '-name' ); ?>"><?php echo esc_html( __( 'Name', 'contact-form-7' ) ); ?></label></th>
+	<td><input type="text" name="name" class="tg-name oneline" id="<?php echo esc_attr( $args['content'] . '-name' ); ?>" /></td>
+	</tr>
+
+	<tr>
+	<th scope="row"><label for="<?php echo esc_attr( $args['content'] . '-limit' ); ?>"><?php echo esc_html( __( "File size limit (bytes)", 'contact-form-7' ) ); ?></label></th>
+	<td><input type="text" name="limit" class="filesize oneline option" id="<?php echo esc_attr( $args['content'] . '-limit' ); ?>" /></td>
+	</tr>
+
+	<tr>
+	<th scope="row"><label for="<?php echo esc_attr( $args['content'] . '-filetypes' ); ?>"><?php echo esc_html( __( 'Acceptable file types', 'contact-form-7' ) ); ?></label></th>
+	<td><input type="text" name="filetypes" class="filetype oneline option" id="<?php echo esc_attr( $args['content'] . '-filetypes' ); ?>" /></td>
+	</tr>
+
+	<tr>
+	<th scope="row"><label for="<?php echo esc_attr( $args['content'] . '-id' ); ?>"><?php echo esc_html( __( 'Id attribute', 'contact-form-7' ) ); ?></label></th>
+	<td><input type="text" name="id" class="idvalue oneline option" id="<?php echo esc_attr( $args['content'] . '-id' ); ?>" /></td>
+	</tr>
+
+	<tr>
+	<th scope="row"><label for="<?php echo esc_attr( $args['content'] . '-class' ); ?>"><?php echo esc_html( __( 'Class attribute', 'contact-form-7' ) ); ?></label></th>
+	<td><input type="text" name="class" class="classvalue oneline option" id="<?php echo esc_attr( $args['content'] . '-class' ); ?>" /></td>
+	</tr>
+
+</tbody>
+</table>
+</fieldset>
+</div>
+
+<div class="insert-box">
+	<input type="text" name="<?php echo $type; ?>" class="tag code" readonly="readonly" onfocus="this.select()" />
+
+	<div class="submitbox">
+	<input type="button" class="button button-primary insert-tag" value="<?php echo esc_attr( __( 'Insert Tag', 'contact-form-7' ) ); ?>" />
+	</div>
+
+	<br class="clear" />
+
+	<p class="description mail-tag"><label for="<?php echo esc_attr( $args['content'] . '-mailtag' ); ?>"><?php echo sprintf( esc_html( __( "To attach the file uploaded through this field to mail, you need to insert the corresponding mail-tag (%s) into the File Attachments field on the Mail tab.", 'contact-form-7' ) ), '<strong><span class="mail-tag"></span></strong>' ); ?><input type="text" class="mail-tag code hidden" readonly="readonly" id="<?php echo esc_attr( $args['content'] . '-mailtag' ); ?>" /></label></p>
+</div>
+<?php
+}
