5fc8ec5e9176c8f6874a3389b3d4db98415c179e
Yulinn233 <977869645@qq.com>
2022-02-10 00:38:28 +0800

    前端(下载页面)：样式布局重构

--- wp-content/themes/ui/pages/download.php
+++ wp-content/themes/ui/pages/download.php
@@ -4,75 +4,193 @@
  */
 get_header();
 ?>
+<style>
+    .help .card-text {
+        min-height: 131px;
+        font-size: .875em;
+    }
+    .py-7 {
+        padding-top: 7rem !important;
+        padding-bottom: 7rem !important;
+    }
+    .shape {
+        pointer-events: none;
+        position: absolute;
+    }
+    .shape-bottom {
+        bottom: 0;
+        left: 0;
+        right: 0;
+    }
+    .shape:not([class*=shape-blur]) {
+        overflow: hidden;
+    }
+    .shape:not([class*=shape-blur])>* {
+        transform: scale(2);
+    }
+    .shape-fluid-x>* {
+        height: auto;
+        width: 100%;
+    }
+    .shape-bottom>* {
+        transform-origin: top center;
+    }
+    .deviation i {
+        width: 1px;
+        height: 10px;
+        background: #000;
+        opacity: 0.8;
+        margin: 0 14px;
+        display: inline-block;
+    }
+    .dot {
+        width: .625rem;
+        height: .625rem;
+        border-radius: 50%;
+        display: inline-block;
+        margin-right: .5rem;
+    }
+</style>
+    <div class="text-center bg-overlay-dark-7 py-7 " style="background-size:cover; background-position: center center;background: #0274be;">
+        <div class="container">
+            <div class="row all-text-white">
+                <div class="col-md-12 align-self-center text-white">
+                                        <h1 class="fw-bold">即刻接入本土生态体系</h1>
+                                        <h6 class="mb-5">一个崭新的独属于中国大陆的 WordPress 生态体系等待你的探索为了方便体验，我们准备了两个方案供你选择</h6>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="position-relative">
+        <div class="shape shape-bottom shape-fluid-x text-light">
+            <svg viewBox="0 0 2880 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 48h2880V0h-720C1442.5 52 720 0 720 0H0v48z" fill="#f0f2f5"></path></svg>      </div>
+    </div>
     <main class="wp-body">
         <div class="container">
             <div class="row">
-                <article class="col-xl-9">
+                <article class="col">
                     <section class="theme-boxshadow wp-banner bg-white p-4">
-                        <h2 class="text-center">
-                            <span class="heading-text">获取 LitePress</span>
+                        <div class="row">
+                            <div class="col-xl-8">
+                        <h2 class="">
+                            <span class="heading-text">选项一：安装发行版</span>
                         </h2>
-                        <p class="">LitePress 是专为中国特殊的互联网环境而适配的 WordPress 衍生版，我们相信 LitePress 可以让你的网站在国内变的更好用。<br/>我们提供了三种方式帮助你体验 LitePress：
+                        <p class="">使用 LitePress 的安装包直接部署一个全新站点是接入本土生态体系最简单便捷的方式。同时，你可以在体验状态不佳时方便的切换回 WordPress。
                         </p>
-                        <h5 class="text-primary mt-2">
-                            直接使用发行版安装包安装
-                        </h5>
+                        <a href="download.php" class="py-3 d-inline-block">安装方法</a>
+                        <p class="">
+                            推荐服务器配置
+                        </p>
+                                <div class="row fs--1 fw-semi-bold text-500 mt-3 gy-3 mt-3 mt-xxl-2">
+                                    <div class="col-xl-4 d-flex align-items-center pe-3"><span class="dot bg-primary"></span><span>php 7.4 及以上</span></div>
+                                    <div class="col-xl-4 d-flex align-items-center pe-3"><span class="dot bg-primary"></span><span>MySQL 5.6 及以上</span></div>
+                                    <div class="col-12 d-flex align-items-center pe-3"><span class="dot  bg-primary"></span><span>Nginx / Apache</span></div>
+
+                                </div>
+                            </div>
+                            <div class="col-xl-4 text-center">
+                                <a href="https://rat.dev/litepress/LitePress/archive/refs/heads/5.8.3.zip" class="btn btn-primary my-3 pe-4">
+                                    <section class="d-flex align-items-center">
+                                        <span class="fa-stack me-2">  <i class="far fa-circle fa-stack-2x"></i><i class="fas fa-long-arrow-down fa-stack-1x"></i></span>
+                                        <article class="text-start"><p>下载</p>
+                                        </article>
+                                    </section>
+                                </a>
+                                <p class="deviation mt-3 text-muted">发行日志<i></i>版本号：v 5.8.3</p>
+                            </div>
+                        </div>
+<hr>
+                        <div class="row">
+                            <div class="col-xl-8">
+                        <h2 class="mt-2">
+                            <span class="heading-text">选项二：使用插件</span>
+
+                        </h2>
                         <p>
-                            你将从头安装一个 LItePress 站点，其安装过程与 WordPress 相同
-                        </p>
-                        <a href="https://rat.dev/litepress/LitePress/archive/refs/heads/5.8.3.zip" class="btn btn-primary my-3 pe-4">
-                            <section class="d-flex align-items-center">
-                                <span class="fa-stack me-2">  <i class="far fa-circle fa-stack-2x"></i><i class="fas fa-long-arrow-down fa-stack-1x"></i></span>
-                                <article class="text-start"><p>立即获取</p>
-                                </article>
-                            </section>
-                        </a>
-                        <h5 class="text-primary mt-2">
-                            在现有的 WordPress 站点上对接本土生态体系
-                        </h5>
-                        <p>
-                            如果你还没准备好立即使用 LitePress，则可以利用 WP-China-Yes 插件在已有的 WordPress 站点中对接 LitePress.cn 所提供的本土生态体系及资源，从而改善 WordPress 站点在 国内的使用体验。
-                        </p>
-                        <a href="https://rat.dev/litepress/LitePress/archive/refs/heads/5.8.3.zip" class="btn btn-primary my-3 pe-4">
-                            <section class="d-flex align-items-center">
+                            对于已有 WordPress 网站的用户可以选择使用 WP-China-Yes 插件来将网站接入到本土生态体系中，同时也可以在插件中一键将 WordPress 转换为 LitePress。</p>
+                            </div>
+                        <div class="col-xl-4 text-center">
+                            <a href="https://rat.dev/litepress/LitePress/archive/refs/heads/5.8.3.zip" class="btn btn-primary my-3 pe-4">
+                                <section class="d-flex align-items-center">
                                 <span class="fa-stack me-2">  <i class="far fa-circle fa-stack-2x"></i><i
-                                        class="fa fa-plug" aria-hidden="true"></i></span>
-                                <article class="text-start"><p>下载 WP-China-Yes 插件</p>
-                                </article>
-                            </section>
-                        </a>
-                        <h5 class="text-primary mt-2">
-                            替换现有的 WordPress 安装
-                        </h5>
-                        <p>
-                            如果你已经有了一个 WordPress 站点，想将其替换为 LitePress，则可以按如下三步操作：
-                            <br>
-                            1、安装 WP-China-Yes 插件
-                        </p>
-                        <a href="https://rat.dev/litepress/LitePress/archive/refs/heads/5.8.3.zip" class="btn btn-primary my-3 pe-4">
-                            <section class="d-flex align-items-center">
-                                <span class="fa-stack me-2">  <i class="far fa-circle fa-stack-2x"></i><i
-                                        class="fa fa-plug" aria-hidden="true"></i></span>
-                                <article class="text-start"><p>下载 WP-China-Yes 插件</p>
-                                </article>
-                            </section>
-                        </a>
-                        <p>
-                            2、提交版本升级请求
-                        </p>
-                        <a href="https://rat.dev/litepress/LitePress/archive/refs/heads/5.8.3.zip" class="btn btn-primary my-3 pe-4">
-                            <section class="d-flex align-items-center">
-                                <span class="fa-stack me-2"> <i class=" fad fa-users-medical"></i></span>
-                                <article class="text-start"><p>申请升级</p>
-                                </article>
-                            </section>
-                        </a>
-                        <p>
-                            3、在站点后台重新安装即可（如下图所示）
-                        </p>
+                                            class="fa fa-plug" aria-hidden="true"></i></span>
+                                    <article class="text-start"><p>下载</p>
+                                    </article>
+                                </section>
+                            </a>
+                            <p class="deviation mt-3 text-muted">发行日志<i></i>版本号：v 5.8.3</p>
+                        </div>
+                        </div>
+            </div>
+
                     </section>
+                    <section class="py-8 border-bottom bg-gradient-light-white">
+                        <div class="container">
+                            <div class="row mb-5">
+                                <h2 class="text-center">与核心团队交流</h2>
+                                <p class="text-center text-muted">
+                                    项目核心团队，包括一个三人的开发小组和社区理事会成员，你可以通过以下两种方式与我们取得联系</p>
+                            </div>
+
+                            <div class="row mb-6 row-cols-1 row-cols-lg-3 gy-3 m-auto help" style="">
+                                <div class="col">
+
+
+                                    <div class="card py-3  theme-boxshadow ">
+
+                                        <div class="card-body text-center">
+                                            <h5 class="card-title ">帮助我们推广</h5>
+                                            <p class="card-text text-muted">
+                                            如果你认可我们在做的事情<br>希望可以帮助此项目推广<br>更多的流量意味着更健康的发展</p>
+                                            <a class="btn btn-primary mt-3" href="/forums">前往论坛 <i class="fad fa-user-friends" aria-hidden="true"></i></a>
+                                        </div>
+                                    </div>
+
+
+                                </div>
+
+                                <div class="col">
+
+
+                                    <div class="card py-3 theme-boxshadow">
+
+                                        <div class="card-body text-center">
+                                            <h5 class="card-title ">参与开发</h5>
+                                            <p class="card-text text-muted">我们欢迎任何水平的程序员、设计师<br>参与到项目的开发工作中<br>我们可以共同构建起这一切。</p>
+                                            <a class="btn btn-primary mt-3" href="https://jq.qq.com/?_wv=1027&amp;k=AizcubYC">前往 QQ 群 <i class="fab fa-qq"></i></a>
+                                        </div>
+                                    </div>
+
+
+                                </div>
+                                <div class="col">
+
+
+                                    <div class="card py-3 theme-boxshadow">
+
+                                        <div class="card-body text-center">
+                                            <h5 class="card-title ">参与翻译</h5>
+                                            <p class="card-text text-muted">当前有大量资源亟待翻译<br>你的参与可以加速这一过程<br>同时相较于 WordPress 社区的翻译工作方式<br>在这里翻译贡献者可以向开发团队<br>请求为平台添加新的特性以优化翻译过程<br>所有请求都会得到快速响应。</p>
+                                            <a href="create" target="_blank" class="btn btn-primary mt-3">
+                                                <i class=" fad fa-users-medical"></i>
+                                                <span class="uabb-button-text uabb-creative-button-text">参与翻译</span>
+                                            </a>
+                                        </div>
+                                    </div>
+
+
+                                </div>
+
+
+                            </div>
+
+
+                        </div> <!-- / .container -->
+                    </section>
+
                 </article>
-                <aside class="col-xl-3 wp-aside ">
+                <!--<aside class="col-xl-3 wp-aside ">
 
                     <section class="theme-boxshadow bg-white ">
                         <header class="  d-flex aside-header align-items-center">
@@ -121,7 +239,7 @@ get_header();
                         </article>
 
                     </section>
-                </aside>
+                </aside>-->
             </div>
         </div>
     </main>
