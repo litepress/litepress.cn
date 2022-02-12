d969cb93c08750b53a03a54bae65f4441062828f
sunxiyuan <sxy@ibadboy.net>
2022-02-12 16:26:58 +0800

    重构(UI 主题):页头模板接入新的子模板体系

--- wp-content/themes/ui/sub-templates/manual-header.php
+++ wp-content/themes/ui/sub-templates/manual-header.php
@@ -7,11 +7,12 @@
     <div class="container">
         <div class="row align-items-center project-row">
             <div class="col-xl-6 ">
-                <h1><span>文档平台</span></h1></div>
+                <h1><span>手册资源</span></h1></div>
             <div class="col-xl-6">
                 <div class="search-form  row justify-content-end">
-                    <form class="col-xl-8" method="get"action="/search">
-                        <input placeholder="文档搜索..." name="keyword" type="search" value="" id="projects-filter" class="filter-search"
+                    <form class="col-xl-8" method="get" action="/search">
+                        <input placeholder="手册搜索..." name="keyword" type="search" value="" id="projects-filter"
+                               class="filter-search"
                         >
                         <input type="hidden" name="tag_id" value="11"/>
                     </form>
@@ -22,6 +23,6 @@
 </header>
 <section class="breadcrumb">
     <div class="container">
-        <?php lpcn_breadcrumb(); ?>
+		<?php lpcn_breadcrumb(); ?>
     </div>
 </section>
