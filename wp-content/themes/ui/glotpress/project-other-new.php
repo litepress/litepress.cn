8c48187f11d4339c00de16bbc7dd1bd63763bb57
sunxiyuan <sxy@ibadboy.net>
2022-02-17 16:17:59 +0800

    特性(翻译平台):申请创建翻译项目必须先登录

--- wp-content/themes/ui/glotpress/project-other-new.php
+++ wp-content/themes/ui/glotpress/project-other-new.php
@@ -14,41 +14,42 @@ gp_tmpl_header();
     </div>
     <div class="container ">
         <div class="setting mb-4">
-        <h2><?php _e( '申请托管第三方项目', 'glotpress' ); ?></h2>
-        <?php /*echo do_shortcode('[contact-form-7 id="1766" title="翻译托管申请"]') */?>
+            <h2><?php _e( '申请托管第三方项目', 'glotpress' ); ?></h2>
+			<?php /*echo do_shortcode('[contact-form-7 id="1766" title="翻译托管申请"]') */ ?>
+			<?php if ( is_user_logged_in() ): ?>
+                <form style="max-width: 640px" class="trusteeship_form needs-validation">
 
-            <form style="max-width: 640px" class="trusteeship_form needs-validation">
-
-                <div class="form-floating  mb-3">
-                    <input type="text" class="form-control" id="tf_project_name" placeholder="项目名称" required>
-                    <label for="">项目名称</label>
-                    <div class="invalid-feedback">
-                        请填写此处
+                    <div class="form-floating  mb-3">
+                        <input type="text" class="form-control" id="tf_project_name" placeholder="项目名称" required>
+                        <label for="">项目名称</label>
+                        <div class="invalid-feedback">
+                            请填写此处
+                        </div>
                     </div>
-                </div>
 
-                <div class="form-floating mb-3">
-                    <input type="text" class="form-control" id="tf_project_textdomin" placeholder="文本域" required>
-                    <label for="">文本域 （指 TextDomin，通常为项目的 slug，但也有例外。请务必正确填写）</label>
-                    <div class="invalid-feedback">
-                        请填写此处
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control" id="tf_project_textdomin" placeholder="文本域" required>
+                        <label for="">文本域 （指 TextDomin，通常为项目的 slug，但也有例外。请务必正确填写）</label>
+                        <div class="invalid-feedback">
+                            请填写此处
+                        </div>
                     </div>
-                </div>
 
-                <div class="mb-3">
-                    <label for="formFile" class="form-label">图标</label>
-                    <input class="form-control" type="file" id="tf_project_icon">
-                </div>
+                    <div class="mb-3">
+                        <label for="formFile" class="form-label">图标</label>
+                        <input class="form-control" type="file" id="tf_project_icon">
+                    </div>
 
-                <div class="form-floating mb-3">
-                    <textarea class="form-control" placeholder="Leave a comment here" id="tf_project_textarea" style="height: 100px"></textarea>
-                    <label for="floatingTextarea2">简介</label>
-                </div>
-                <button type="submit" class="btn btn-primary">提交</button>
-            </form>
-
-
-
+                    <div class="form-floating mb-3">
+                        <textarea class="form-control" placeholder="Leave a comment here" id="tf_project_textarea"
+                                  style="height: 100px"></textarea>
+                        <label for="floatingTextarea2">简介</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">提交</button>
+                </form>
+			<?php else: ?>
+                你需要先 <a href="/login">登录</a>
+			<?php endif; ?>
         </div>
     </div>
 <?php
