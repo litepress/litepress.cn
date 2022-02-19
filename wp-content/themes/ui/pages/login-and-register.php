b298146feb8c014396ff0474e809589e52679294
Yulinn233 <977869645@qq.com>
2022-02-19 23:57:42 +0800

    前端(litepress)：间距问题

--- wp-content/themes/ui/pages/login-and-register.php
+++ wp-content/themes/ui/pages/login-and-register.php
@@ -13,7 +13,7 @@ get_header();
 
 
 
-<main class="form-signin bg-white theme-boxshadow">
+<main class="form-signin bg-white theme-boxshadow my-4">
 
         <?php
         $custom_logo_id = get_theme_mod( 'custom_logo' );
