1729ad52799e80368be82663146bda422a440451
sunxiyuan <sxy@ibadboy.net>
2022-02-19 19:20:59 +0800

    修复(发行版升级插件):规范 rest api

--- wp-content/plugins/lpcn-lp-apply/lpcn-lp-apply.php
+++ wp-content/plugins/lpcn-lp-apply/lpcn-lp-apply.php
@@ -22,59 +22,60 @@ $apply_site_list = get_site_option( 'lp_apply_site', array() );
 
 
 // 为后台加上站点统计
-if (is_admin()) {
-    $site = '';
-    $lp_count = 0;
-    $wp_count = 0;
-    foreach ($apply_site_list as $k => $v) {
-        
-        /**$check = strstr($k, 'haoziwl');
-        if ($check) {
-            unset($apply_site_list[$k]);
-        }*/
-        
-        if ($v == 1) {
-            $site_type = 'LitePress';
-            $lp_count++;
-        } else {
-            $site_type = 'WordPress';
-            $wp_count++;
-        }
-        $site .= $k . ' --> '. $site_type . '<br>';
-    }
-    
-    /*update_site_option('lp_apply_site', $apply_site_list);*/
-    $site .= '合计：' . count($apply_site_list) . '个站点，其中LitePress站点有：' . $lp_count . '个，WordPress站点有：' . $wp_count . '个。';
-    
-    // 建立设置
-    Framework::createSection( $prefix ?? 'litepress', array(
-    	'id'     => 'apply',
-    	'title'  => 'Apply',
-    	'icon'   => 'fa fa-code',
-    	'fields' => array(
-    		array(
-    			'type'  => 'content',
-    			'content' => $site,
-    		),
-    	)
-    ) );
+if ( is_admin() ) {
+	$site     = '';
+	$lp_count = 0;
+	$wp_count = 0;
+	foreach ( $apply_site_list as $k => $v ) {
+
+		/**$check = strstr($k, 'haoziwl');
+		 * if ($check) {
+		 * unset($apply_site_list[$k]);
+		 * }*/
+
+		if ( $v == 1 ) {
+			$site_type = 'LitePress';
+			$lp_count ++;
+		} else {
+			$site_type = 'WordPress';
+			$wp_count ++;
+		}
+		$site .= $k . ' --> ' . $site_type . '<br>';
+	}
+
+	/*update_site_option('lp_apply_site', $apply_site_list);*/
+	$site .= '合计：' . count( $apply_site_list ) . '个站点，其中LitePress站点有：' . $lp_count . '个，WordPress站点有：' . $wp_count . '个。';
+
+	// 建立设置
+	Framework::createSection( $prefix ?? 'litepress', array(
+		'id'     => 'apply',
+		'title'  => 'Apply',
+		'icon'   => 'fa fa-code',
+		'fields' => array(
+			array(
+				'type'    => 'content',
+				'content' => $site,
+			),
+		)
+	) );
 }
 
 add_action( 'rest_api_init', '\LitePress\Apply\lp_apply_register_rest_route' );
 function lp_apply_register_rest_route() {
 	register_rest_route( 'lp/', 'apply', array(
-		'methods'  => WP_REST_Server::CREATABLE,
-		'callback' => '\LitePress\Apply\lp_apply',
+		'methods'             => WP_REST_Server::CREATABLE,
+		'callback'            => '\LitePress\Apply\lp_apply',
+		'permission_callback' => '__return_true',
 	) );
 	register_rest_route( 'lp/', 'exit', array(
-		'methods'  => WP_REST_Server::CREATABLE,
-		'callback' => '\LitePress\Apply\lp_exit',
+		'methods'             => WP_REST_Server::CREATABLE,
+		'callback'            => '\LitePress\Apply\lp_exit',
+		'permission_callback' => '__return_true',
 	) );
 }
 
 function lp_apply( WP_REST_Request $request ): WP_REST_Response {
-
-    global $apply_site_list;
+	global $apply_site_list;
 
 	$params  = $request->get_params();
 	$request = request( $params['site'] );
@@ -155,20 +156,20 @@ function lp_exit( WP_REST_Request $request ): WP_REST_Response {
 
 			if ( $apply_site_list[ $params['site'] ] ?? 0 ) {
 
-                if ( $apply_site_list[ $params['site'] ] == 'exit' ) {
-                    $args = array(
-    					'code' => 1,
-    					'msg'  => '此站点已退出，无需重复退出！',
-    				);
-                } else {
-                    $apply_site_list[ $params['site'] ] = 'exit';
-    			    update_site_option( 'lp_apply_site', $apply_site_list );
-    
-    				$args = array(
-    					'code' => 0,
-    					'msg'  => '退出成功！感谢您体验LitePress。',
-    				);
-                }
+				if ( $apply_site_list[ $params['site'] ] == 'exit' ) {
+					$args = array(
+						'code' => 1,
+						'msg'  => '此站点已退出，无需重复退出！',
+					);
+				} else {
+					$apply_site_list[ $params['site'] ] = 'exit';
+					update_site_option( 'lp_apply_site', $apply_site_list );
+
+					$args = array(
+						'code' => 0,
+						'msg'  => '退出成功！感谢您体验LitePress。',
+					);
+				}
 
 			} else {
 				$args = array(
