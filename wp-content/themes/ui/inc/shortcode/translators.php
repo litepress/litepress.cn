b9dbe92f7987b58bfda158a65e65759d0f9e2c92
sunxiyuan <sxy@ibadboy.net>
2022-02-20 12:45:09 +0800

    特性(社区论坛):翻译贡献者榜单添加周榜，目前只完成了功能，等待编写前端界面

--- wp-content/themes/ui/inc/shortcode/translators.php
+++ wp-content/themes/ui/inc/shortcode/translators.php
@@ -10,23 +10,65 @@ function wcy_get_translators() {
 
 	global $wpdb;
 
-	$translators = wp_cache_get( 'translators', 'litepress-cn' );
-	if ( empty( $translators ) ) {
-		$translators = $wpdb->get_results( 'select user_id, count(*) as count from wp_4_gp_translations where user_id!=517 and user_id!=0 group by user_id order by count desc;' );
-		wp_cache_set( 'translators', $translators, 'litepress-cn', 86400 );
+	/**
+	 * 获取总榜数据
+	 */
+	$translators_all = wp_cache_get( 'translators-all', 'litepress-cn' );
+	if ( empty( $translators_all ) ) {
+		$sql = <<<SQL
+select user_id, count(*) as count
+from wp_4_gp_translations
+where user_id != 517
+  and user_id != 0
+group by user_id
+order by count desc
+limit 10;
+SQL;
+
+		$translators_all = $wpdb->get_results( $sql );
+		wp_cache_set( 'translators-all', $translators_all, 'litepress-cn', 86400 );
 	}
 
-	$i = 0;
-	foreach ( $translators as $k => $v ) {
-		if ( $i >= 10 ) {
-			break;
-		}
+	/**
+	 * 获取周榜数据
+	 */
+	$translators_week = wp_cache_get( 'translators-week', 'litepress-cn' );
+	if ( empty( $translators_week ) ) {
+		$sql = <<<SQL
+select user_id, count(*) as count
+from wp_4_gp_translations
+where user_id != 517
+  and user_id != 0
+
+group by user_id
+order by count desc
+limit 10;
+SQL;
+
+		$translators_week = $wpdb->get_results( $sql );
+		wp_cache_set( 'translators-week', $translators_week, 'litepress-cn', 86400 );
+	}
+
+	/**
+	 * 输出周榜
+	 */
+	$html .= '周榜';
+	foreach ( $translators_week as $k => $v ) {
 		$user_info = get_user_by( 'id', $v->user_id );
-		if ( $user_info) {
+		if ( $user_info ) {
 			$html .= sprintf( '<li><em>%d.</em> <div class="rank-list__name"><a href="/user/%s?profiletab=translate">%s%s</a></div><span class="rank-list__number">%d 条</span></li>', $k + 1, $user_info->data->user_login, get_avatar( $user_info->data->user_email, 32 ), $user_info->data->display_name, $v->count );
 		}
+	}
 
-		$i ++;
+	/**
+	 * 输出总榜
+	 */
+	$html .= '总榜';
+	foreach ( $translators_all as $k => $v ) {
+		$user_info = get_user_by( 'id', $v->user_id );
+		if ( $user_info ) {
+			$html .= sprintf( '<li><em>%d.</em> <div class="rank-list__name"><a href="/user/%s?profiletab=translate">%s%s</a></div><span class="rank-list__number">%d 条</span></li>', $k + 1, $user_info->data->user_login, get_avatar( $user_info->data->user_email, 32 ), $user_info->data->display_name, $v->count );
+		}
 	}
 
 	return $html .= '</ul>';
