46e8e032b2d0642ffcd3f90787779d7949ebe2a6
Yulinn233 <977869645@qq.com>
2022-02-20 14:25:24 +0800

    前端(litepress)：申请第三方表单 bug 修复

--- wp-content/themes/ui/glotpress/project-other-new.php
+++ wp-content/themes/ui/glotpress/project-other-new.php
@@ -36,13 +36,12 @@ gp_tmpl_header();
                     </li>
                     <li class="mb-3">
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="plugin" value="plugin" required>
-                        <label class="form-check-label" for="inlineRadio1">插件</label>
-
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="plugin" value="plugin" checked required>
+                        <label class="form-check-label" for="plugin">插件</label>
                     </div>
                     <div class="form-check form-check-inline">
                         <input class="form-check-input" type="radio" name="inlineRadioOptions" id="theme" value="theme" required>
-                        <label class="form-check-label" for="inlineRadio2">主题</label>
+                        <label class="form-check-label" for="theme">主题</label>
                     </div>
                     </li>
                     <hr>
