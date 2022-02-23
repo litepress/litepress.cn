5a8a4a72d1acb45edc0d4219600a7e963372510c
Yulinn233 <977869645@qq.com>
2022-02-24 00:34:09 +0800

    前端(litepress)：恢复

--- wp-content/themes/ui/bbpress/form-topic.php
+++ wp-content/themes/ui/bbpress/form-topic.php
@@ -94,12 +94,12 @@ if ( ! bbp_is_single_forum() ) : ?>
 
 						<?php do_action( 'bbp_theme_before_topic_form_content' ); ?>
 
-						<?php bbp_the_content( array( 'context' => 'topic' ) ); ?>
-<!--                        <textarea class="bbp-the-content wp-editor-area" rows="12" tabindex="1" autocomplete="off" cols="40" name="bbp_topic_content" id="bbp_topic_content" aria-hidden="true" style="display: none;"></textarea>
+						<?php /*bbp_the_content( array( 'context' => 'topic' ) ); */?>
+                        <textarea class="bbp-the-content wp-editor-area" rows="12" tabindex="1" autocomplete="off" cols="40" name="bbp_topic_content" id="bbp_topic_content" aria-hidden="true" style="display: none;"></textarea>
                         <section class="wang-editor">
                             <div id="bbp-editor-toolbar" class="editor-toolbar"></div>
                             <div id="bbp-editor-container" style="height:301px" class="editor-container heti"></div>
-                        </section>-->
+                        </section>
 						<?php do_action( 'bbp_theme_after_topic_form_content' ); ?>
 
 						<?php if ( ! ( bbp_use_wp_editor() || current_user_can( 'unfiltered_html' ) ) ) : ?>
