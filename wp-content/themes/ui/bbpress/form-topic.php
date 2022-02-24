ce4d41c7dd20da20b8470d18bf22559392e10696
Yulinn233 <977869645@qq.com>
2022-02-24 23:59:47 +0800

    前端(litepress)：解决编辑器不会填充P便签的bug

--- wp-content/themes/ui/bbpress/form-topic.php
+++ wp-content/themes/ui/bbpress/form-topic.php
@@ -94,9 +94,9 @@ if ( ! bbp_is_single_forum() ) : ?>
 
 						<?php do_action( 'bbp_theme_before_topic_form_content' ); ?>
 
-						<?php /*bbp_the_content( array( 'context' => 'topic' ) ); */?>
+						 <?php /*bbp_the_content( array( 'context' => 'topic' ) ); */?>
                         <textarea class="bbp-the-content wp-editor-area"  autocomplete="off" cols="40" name="bbp_topic_content" id="bbp_topic_content" aria-hidden="true" style="display: none;"></textarea>
-                        <div class="d-none bbp_topic_content_hide"><?php bbp_form_topic_content(); ?></div>
+                        <div class="d-none bbp_topic_content_hide"><?php bbp_topic_content(); ?></div>
                         <section class="wang-editor">
                             <div id="bbp-editor-toolbar" class="editor-toolbar"></div>
                             <div id="bbp-editor-container" style="height:301px" class="editor-container heti"></div>
