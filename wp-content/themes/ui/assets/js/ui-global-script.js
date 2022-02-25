091b66fc25f2eb032636de4a5ebdeab7f4789b55
Yulinn233 <977869645@qq.com>
2022-02-25 12:24:02 +0800

    前端(litepress)：新增评论回复编辑同步内容

--- wp-content/themes/ui/assets/js/ui-global-script.js
+++ wp-content/themes/ui/assets/js/ui-global-script.js
@@ -401,6 +401,7 @@ const toolbar = E.createToolbar({
     config: toolbarConfig,
     mode: 'simple'
 })
+
 }
 
 if ( $("#bbp-editor-container").length > 0 ) {
@@ -442,14 +443,13 @@ if ( $("#bbp-editor-container").length > 0 ) {
     })
     editor.on('fullScreen', () => { $("body").addClass("overflow-hidden") })
     editor.on('unFullScreen', () => { $("body").removeClass("overflow-hidden") })
+    /*话题编辑同步内容*/
     $(function() {
         const bbp_content = $(".topic-edit .bbp_topic_content_hide").html();
-
         const pre_class = $(".topic-edit .bbp_topic_content_hide pre").attr("class");
         if (pre_class !== undefined ){
             const pre_class_1 = pre_class.replace(/^\s*/,"");
             const C1 = bbp_content.replace("tabindex=\"0\"","")
-            /*const C4 = C1.replace(/class\s*?=\s*?([‘"])[\s\S]*?\1/g, '')*/
             const C4 = C1.replaceAll(pre_class, pre_class_1)
             editor.dangerouslyInsertHtml(C4)
         }else {
@@ -457,7 +457,20 @@ if ( $("#bbp-editor-container").length > 0 ) {
         }
 
     })
+    /*评论编辑同步内容*/
+    $(function() {
+        const bbp_content = $(".reply-edit .bbp_topic_content_hide").html();
+        const pre_class = $(".reply-edit .bbp_topic_content_hide pre").attr("class");
+        if (pre_class !== undefined ){
+            const pre_class_1 = pre_class.replace(/^\s*/,"");
+            const C1 = bbp_content.replace("tabindex=\"0\"","")
+            const C4 = C1.replaceAll(pre_class, pre_class_1)
+            editor.dangerouslyInsertHtml(C4)
+        }else {
+            editor.dangerouslyInsertHtml(bbp_content)
+        }
 
+    })
 }
 /*搜索占位符*/
 const projectsearch = $(".search-form input[type=search]");
