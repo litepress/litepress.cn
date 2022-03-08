81b65892be647e5dc9e55b21750fc5349e8a8188
Yulinn233 <977869645@qq.com>
2022-03-08 21:52:11 +0800

    前端（弹窗登录）：忘记密码添加新窗口打开

--- wp-content/themes/ui/footer.php
+++ wp-content/themes/ui/footer.php
@@ -51,7 +51,7 @@
                                     <label class="form-check-label" for="rememberme">保持登录状态</label>
                                 </div>
                                 <div class=" col text-end">
-                                    <a href="/password-reset">忘记密码？</a>
+                                    <a target="_blank"  href="/password-reset">忘记密码？</a>
                                 </div>
                             </div>
                             <button class="w-100 btn btn-lg btn-primary" data-type="submit" type="button">
