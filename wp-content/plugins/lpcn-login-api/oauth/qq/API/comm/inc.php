a5fda4cb9855b2e470d00848debbe9a7d1715975
sunxiyuan <sxy@ibadboy.net>
2022-03-13 18:19:24 +0800

    特性(用户认证):更新 QQ 互联配置信息

---
+++ wp-content/plugins/lpcn-login-api/oauth/qq/API/comm/inc.php
@@ -0,0 +1,2 @@
+<?php die('forbidden'); ?>
+{"appid":"101996049","appkey":"4aa35524b36d11024435f6814cabf5c3","callback":"example/oauth/callback.php","scope":"get_user_info","errorReport":true,"storageType":"file","host":"localhost","user":"root","password":"root","database":"test"}
--- wp-content/plugins/lpcn-login-api/oauth/qq/example/oauth/index.php
+++ wp-content/plugins/lpcn-login-api/oauth/qq/example/oauth/index.php
@@ -1,9 +1,5 @@
 <?php
 
-ini_set( 'display_errors', 1 );
-error_reporting( E_ERROR | E_WARNING | E_PARSE );
-
 require_once("../../API/qqConnectAPI.php");
-
 $qc = new QC();
 $qc->qq_login();
