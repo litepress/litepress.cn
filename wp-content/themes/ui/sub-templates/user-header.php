15eb4ca2b0fbe4e9bb7b363981ef0c042bd36b0a
孙锡源 <sxy@ibadboy.net>
2022-05-08 19:56:24 +0800

    特性(UI):为用户中心添加子导航

---
+++ wp-content/themes/ui/sub-templates/user-header.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 文档banner部分
+ */
+?>
+<header class="sub-banner banner">
+    <div class="container">
+        <div class="row align-items-center project-row">
+            <div class="col-xl-6 ">
+                <h1><span>用户中心</span></h1></div>
+            <div class="col-xl-6">
+                <div class="search-form  row justify-content-end">
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<section class="breadcrumb">
+    <div class="container">
+		<?php lpcn_breadcrumb(); ?>
+    </div>
+</section>
