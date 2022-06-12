56e08f9f6125fa3133830ccbda1ae5789850e3cc
孙锡源 <sxy@ibadboy.net>
2022-06-12 22:34:51 +0800

    特性(Cravatar):在新的头像管理页面中引用 React 单页代码

--- wp-content/plugins/cravatar/pages/avatars.php
+++ wp-content/plugins/cravatar/pages/avatars.php
@@ -1,3 +1,12 @@
-<!--头像管理页面-->
-头像管理
+<?php
+/**
+ * 头像管理页面
+ */
 
+use const LitePress\Cravatar\PLUGIN_DIR;
+
+get_header();
+
+readfile( PLUGIN_DIR . '/frontend/dist/index.html' );
+
+get_footer();
