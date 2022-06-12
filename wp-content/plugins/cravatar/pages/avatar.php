ab33ee01ec111c0f047b2c55f0d198d22d2a8808
孙锡源 <sxy@ibadboy.net>
2022-06-12 16:40:48 +0800

    特性(Cravatar):暂存

--- wp-content/plugins/cravatar/pages/avatar.php
+++ wp-content/plugins/cravatar/pages/avatar.php
@@ -70,6 +70,33 @@ HTML;
 }
 
 /**
+ * 发送 HTTP GET 请求
+ *
+ * @param $url
+ *
+ * @return bool|string
+ */
+function http_get($url)
+{
+	$oCurl = curl_init();
+	if (stripos($url, "https://") !== FALSE) {
+		curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
+		curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, FALSE);
+		curl_setopt($oCurl, CURLOPT_SSLVERSION, 1); //CURL_SSLVERSION_TLSv1
+	}
+	curl_setopt($oCurl, CURLOPT_URL, $url);
+	curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1);
+	$sContent = curl_exec($oCurl);
+	$aStatus = curl_getinfo($oCurl);
+	curl_close($oCurl);
+	if (intval($aStatus["http_code"]) == 200) {
+		return $sContent;
+	} else {
+		return false;
+	}
+}
+
+/**
  * 从本地读取远程图片
  *
  * 如果本地存在缓存则直接返回缓存，如果不存在缓存则从远端下载并进行本地缓存
