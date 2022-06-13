a9afbc474b6c1c9efe7f6fe010d76b294f87ad2a
孙锡源 <sxy@ibadboy.net>
2022-06-14 02:56:03 +0800

    特性(Cravatar):使自定义页面支持修改标题

--- wp-content/plugins/cravatar/pages/avatars.php
+++ wp-content/plugins/cravatar/pages/avatars.php
@@ -5,6 +5,12 @@
 
 use const LitePress\Cravatar\PLUGIN_DIR;
 
+add_filter( 'wp_title_parts', function ( $title ) {
+	$title[0] = '头像管理';
+
+	return $title;
+} );
+
 get_header();
 
 readfile( PLUGIN_DIR . '/frontend/dist/index.html' );
