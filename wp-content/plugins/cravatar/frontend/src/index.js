795cbc012e959e8409f3f9d7594762d2830d0b01
Yulinn233 <977869645@qq.com>
2022-06-16 23:20:25 +0800

    [React]头像:删除老版CSS

--- wp-content/plugins/cravatar/frontend/src/index.js
+++ wp-content/plugins/cravatar/frontend/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import './lp-bootstarp.css';
 import App from './App';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import "./assets/fontawesome/css/all.min.css";
-import './lp-bootstarp.css';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
